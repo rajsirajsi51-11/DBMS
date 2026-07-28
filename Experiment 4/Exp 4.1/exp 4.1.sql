934fd7a3bcdbe863c81cac78162f46b6ce941a67
rajsirajsi51-11 <rajsirajsi51@gmail.com>
2026-07-28 12:55:36 +0530

    Create exp 4.1.sql

---
+++ Experiment 4/Exp 4.1/exp 4.1.sql
@@ -0,0 +1,20 @@
+-- Customers and Orders: List the customer_name and order_date for all customers who have placed orders.
+
+Select c.customer_name,o.order_date from 
+orders o
+left join customers c
+ON c.customer_id = o.customer_id;
+
+-- All Customers and Their Orders: List all customer names and their corresponding product_name from orders, if they have any. Include customers even if they haven't placed any orders.
+
+Select c.customer_name,o.product_name 
+from customers c 
+left join orders o
+ON c.customer_id = o.customer_id;
+
+
+-- Find Products and Their Orders: Display Product Name and the order_date from all the products that are ordered.
+
+SELECT p.product_name, o.order_date
+FROM products p
+INNER JOIN orders o ON p.product_name = o.product_name;
